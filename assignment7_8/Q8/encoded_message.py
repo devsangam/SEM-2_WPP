8e9c616bc255026ea308d19d1f724cc404a3ec02
devsangam <u24ai070@aid.svnit.ac.in>
2025-04-21 23:37:16 +0530

    Update encoded_message.py

--- assignment7_8/Q8/encoded_message.py
+++ assignment7_8/Q8/encoded_message.py
@@ -1,4 +1,21 @@
-import string
-codes=[code for code in enumerate(string.ascii_uppercase,1)]
-print(codes)
-encoded_mesage=input("Enter message:\t").split("")
+def all_decodings(s):
+    def backtrack(idx, path_str, path_nums):
+        if idx == len(s):
+            results.append((path_str, path_nums.copy()))
+            return
+        if s[idx] != '0': 
+            num = int(s[idx])
+            backtrack(idx+1, path_str + chr(ord('A') + num - 1),
+                      path_nums + [num])
+        if idx+1 < len(s):
+            num = int(s[idx:idx+2])
+            if 10 <= num <= 26:
+                backtrack(idx+2, path_str + chr(ord('A') + num - 1),
+                          path_nums + [num])
+
+    results = []
+    backtrack(0, "", [])
+    return results
+message=input("Eeter message:\t")
+for decoded, grouping in all_decodings(message):
+    print(f"{decoded}\twith grouping {grouping}")
